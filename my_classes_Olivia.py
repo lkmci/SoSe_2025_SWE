11bdc15087842ad50c84a9cfe49d138366d7a20a
Olivia Kiechl <ko8973@mci4me.at>
2025-04-07 09:12:24 +0200

    Updatet Aufgabe 6.1 Olivia

--- my_classes_Olivia.py
+++ my_classes_Olivia.py
@@ -1,21 +1,34 @@
 class Subject():
     def __init__(self, firstname, last, sex, age):
-      pass
-
+      self.firstname = firstname
+      self.last = last
+      self.sex = sex
+      self.age = age
+    
+    def __str__(self):
+       return f"First: {self.firstname}, Last: {self.last}, Sex: {self.sex}, Age: {self.age}"
+    
     def estimate_max_hr(self):
       """A function that estimates the maximum heart rate of a subject"""
       pass
 
 class Supervisor():
     def __init__(self, first_name, last_name):
-      pass
+      self.first_name = first_name
+
+    def __str__(self):
+       return f"Vorname: {self.first_name}"
 
 class Experiment():
     def __init__(self, name, date):
-      pass
+      self.name = name
+      self.date = date
 
     def add_subject(self, subject):
       self.subject = subject
 
     def add_supervisor(self, supervisor):
-      pass
+      self.supervisor = supervisor
+
+    def __str__(self):
+       return f"Name: {self.name}, Date:{self.date}, Subject:{self.subject}, Supervisor:{self.supervisor}"
