444561e9c9999b6a511d8fa7be1505dea2ef237b
Lena Kurzthaler <kl4213@mci4me.at>
2025-04-07 09:22:01 +0200

    Updated Aufgabe 6.1 - Lena

--- my_classes_lena.py
+++ my_classes_lena.py
@@ -1,21 +1,35 @@
 class Subject():
-    def __init__(self, first_name, last, sex, age):
-      pass
-
+    def __init__(self, firstname, last, sex, age):
+      self.firstname = firstname
+      self.last = last
+      self.sex = sex
+      self.age = age
+    
+    def __str__(self):
+       return f"First: {self.firstname}, Last: {self.last}, Sex: {self.sex}, Age: {self.age}"
+    
     def estimate_max_hr(self):
       """A function that estimates the maximum heart rate of a subject"""
       pass
 
 class Supervisor():
     def __init__(self, first_name, last_name):
-      pass
+      self.first_name = first_name
+      self.last_name = last_name
+
+    def __str__(self):
+       return f"Vorname: {self.first_name}, Nachname: {self.last_name}"
 
 class Experiment():
     def __init__(self, name, date):
-      pass
+      self.name = name
+      self.date = date
 
     def add_subject(self, subject):
       self.subject = subject
 
     def add_supervisor(self, supervisor):
-      pass
+      self.supervisor = supervisor
+
+    def __str__(self):
+       return f"Name: {self.name}, Date:{self.date}, Subject:{self.subject}, Supervisor:{self.supervisor}"
