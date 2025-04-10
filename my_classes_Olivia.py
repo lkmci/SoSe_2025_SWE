44efcbb6e388c2ca44178fd30bde7da32b0de77b
Olivia Kiechl <ko8973@mci4me.at>
2025-04-10 22:44:20 +0200

    Update 6.2 Olivia

--- my_classes_Olivia.py
+++ my_classes_Olivia.py
@@ -1,23 +1,50 @@
-class Subject():
-    def __init__(self, firstname, last, sex, age):
-      self.firstname = firstname
-      self.last = last
-      self.sex = sex
-      self.age = age
+import datetime
+from my_functions import estimate_max_hr
+from my_functions import calculate_and_show_heartrate
+
+
+class Person():
+    def __init__(self, name, sex, date_of_birth):
+        self.name = name
+        self.sex = sex
+        self.__date_of_birth = datetime.datetime.strptime(date_of_birth, "%Y-%m-%d").date()  
+    def get_age_years(self):
+        today = datetime.date.today()
+        age_years = datetime.datetime.now().year - self.__date_of_birth.year
+        
+        
+class Subject(Person):
+    def __init__(self, name, sex, date_of_birth):
+        super().__init__(name, sex, date_of_birth)  
+        self.sex = sex
+        self.__date_of_birth = date_of_birth
     
-    def __str__(self):
-       return f"First: {self.firstname}, Last: {self.last}, Sex: {self.sex}, Age: {self.age}"
+
+    def get_subject_age(self):
+        return self.get_age_years()
     
-    def estimate_max_hr(self):
-      """A function that estimates the maximum heart rate of a subject"""
-      pass
+    def estimate_subject_max_hr(self):
+        from my_functions import estimate_max_hr, calculate_and_show_heartrate
+        """ Berechnet die maximale Herzfrequenz für das Subject """
+        age_years = self.get_subject_age()
+        return estimate_max_hr(age_years, self.sex)
+    
+    def get_subject_info(self): 
+        return {
+            "first_name": self.name,
+            "date_of_birth": self.__date_of_birth,
+            "sex": self.sex, 
+              "estimate_max_hr": self.estimate_subject_max_hr()
+}
 
 class Supervisor():
-    def __init__(self, first_name, last_name):
-      self.first_name = first_name
-      self.last_name = last_name
-    def __str__(self):
-       return f"Vorname: {self.first_name}, Nachname: {self.last_name}"
+    def __init__(self, name, sex, date_of_birth):
+        self.first_name = name 
+        self.date_of_birth = date_of_birth
+
+    def get_supervisor_age(self):
+        return self.get_age_years()
+
 
 class Experiment():
     def __init__(self, name, date):
@@ -32,3 +59,17 @@ class Experiment():
 
     def __str__(self):
        return f"Name: {self.name}, Date:{self.date}, Subject:{self.subject}, Supervisor:{self.supervisor}"
+    
+    def get_experiment_info(self):
+        return {
+            "experiment_name": self.name,
+            "experiment_date": self.date,
+            "subject": self.subject.get_subject_info(),
+            "supervisor": self.supervisor.get_supervisor_age()
+        }
+
+
+# Testen der Klassen
+#p = Subject("Lena", "female", "2006-03-16")
+#print(f"Alter: {p.get_age_years()} Jahre")
+#print(f"Maximale Herzfrequenz: {p.estimate_subject_max_hr()} bpm")
--- test_Olivia.py
+++ test_Olivia.py
@@ -3,9 +3,9 @@ from my_classes_Olivia import Subject, Supervisor, Experiment
 if __name__ == "__main__":
 
     # Erstellen eines Leistungstests
-    supervisor = Supervisor("Olivia", "Kiechl")
-    subject = Subject("Olivia", "Kiechl", "female", 20)
-    subject.estimate_max_hr()
+    supervisor = Supervisor("Olivia", "Kiechl", "date of birth")
+    subject = Subject("Olivia", "Kiechl", "2005-01-18")
+    subject.estimate_subject_max_hr()
 
     experiment = Experiment("Leistungstest", "2021-01-01")
     experiment.add_subject(subject)
